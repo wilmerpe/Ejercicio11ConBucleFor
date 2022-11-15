6b7f46142e4af6c141af4ef75cf5a4cc44616489
wilmerpe <wilmer.wmpac@gmail.com>
2022-11-15 18:52:04 +0100

    Initial commit

---
+++ src/Ejercicio11While.java
@@ -0,0 +1,11 @@
+public class Ejercicio11While {
+    public static void main(String[] args) {
+        long producto = 1;
+
+        for(int i=1; i<=20; i+=2){
+            producto *= i;
+        }
+
+        System.out.println("El producto es " + producto);
+    }
+}
